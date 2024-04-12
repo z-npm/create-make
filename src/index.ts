c8f6da2750f242e3ce04636476d1f92d2932ca5b
Zero <z-nerd@proton.me>
2024-04-12 14:19:55 -0400

    Add needed utils for changing starter content

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
-import { getConfig } from "./config"
-import { projectPrompt } from "./prompt"
-import { gitClone, rmRF } from "./utils"
+// import { getConfig } from "./config"
+// import { projectPrompt } from "./prompt"
+// import { gitClone, rmRF } from "./utils"
 
 const boot = async () => {
-  const config = getConfig()
-  const answer = await projectPrompt(config)
-
-  const PROJECT_PATH = `${process.cwd()}/${answer.projectName}`
-  const PROJECT_GIT_PATH = `${PROJECT_PATH}/.git`
-
-  gitClone(answer.repo, answer.projectName)
-  rmRF(PROJECT_GIT_PATH)
+  // const config = getConfig()
+  // const answer = await projectPrompt(config)
+  //
+  // const PROJECT_PATH = `${process.cwd()}/${answer.projectName}`
+  // const PROJECT_GIT_PATH = `${PROJECT_PATH}/.git`
+  //
+  // gitClone(answer.repo, answer.projectName)
+  // rmRF(PROJECT_GIT_PATH)
 }
 
 export default boot
--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,28 @@
-import { existsSync, mkdirSync, readFileSync, writeFileSync, rmSync } from "fs"
+import {
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  writeFileSync,
+  rmSync,
+  readdirSync,
+  statSync,
+  renameSync,
+} from "fs"
 import { execSync } from "child_process"
+import {
+  join,
+  // dirname,
+  basename,
+} from "path"
 
 export const mkdir = (path: string) => mkdirSync(path, { recursive: true })
 export const rmRF = (path: string) =>
   rmSync(path, { recursive: true, force: true })
 
 export const pathExists = (path: string) => existsSync(path)
+
+export const rename = (oldPath: string, newPath: string) =>
+  renameSync(oldPath, newPath)
 
 export const readFile = (path: string) =>
   readFileSync(path, { encoding: "utf8" })
@@ -21,3 +38,56 @@ export const cmd = (command: string, path: string = process.cwd()) =>
 
 export const gitClone = (repo: string, projectName: string) =>
   cmd(`git clone --depth=1 ${repo} ${projectName}`)
+
+export const findFile = (
+  dir: string,
+  fileName: string | boolean,
+  exclude: string[] = ["node_modules", ".git"],
+) => {
+  let finds: string[] = []
+  readdirSync(dir).forEach((file) => {
+    const filePath = join(dir, file)
+
+    if (!exclude.includes(filePath)) {
+      const fileStat = statSync(filePath)
+      if (fileStat.isDirectory())
+        finds = [...finds, ...findFile(filePath, fileName)]
+      else if (fileName === true) finds.push(filePath)
+      else if (new RegExp("\\b" + fileName + "\\b").test(basename(file)))
+        finds.push(filePath)
+    }
+  })
+
+  return finds
+}
+
+export const filesFindRename = (
+  path: string,
+  oldName: string,
+  newName: string,
+) => {
+  findFile(path, oldName).map((item) => {
+    rename(item, item.replace(oldName, newName))
+  })
+}
+
+export const filesChangeContent = (
+  path: string,
+  _oldName: string,
+  _newName: string,
+) => {
+  findFile(path, true).map((item) => {
+    const content = readFileSync(item, { encoding: "utf-8" })
+    if (new RegExp("\\b" + _oldName + "\\b").test(content)) {
+      writeFileSync(item, content.replace(_oldName, _newName), {
+        encoding: "utf-8",
+      })
+    }
+
+    // let result = ""
+    // content.split(/\n/).forEach((line) => {
+    //   if (new RegExp("\\b" + _oldName + "\\b").test(line)) result += `${line}\n`
+    // })
+    // console.log(item, result)
+  })
+}
