1957cf6833aa0fc14a8d5e65799ae581de1a9dc3
Zero <zero.red.dev@gmail.com>
2026-08-29 18:36:47 +0330

    Update project to support new changes

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "vite"
 import { swc } from "@o.z/vite-plugin-swc"
 import path from "path"
 import { nodeExternals } from "rollup-plugin-node-externals"
-import versionPlugin from "./plugin/vite-plugin-version"
+import versionPlugin from "./plugin/vite-plugin-version.ts"
 
 export default defineConfig({
   build: {
     lib: {
       name: "create-make",
-      entry: [path.resolve(__dirname, "./src/index.ts")],
+      entry: [path.resolve(import.meta.dirname, "./src/index.ts")],
       fileName: (format, name) => {
         if (format === "es") return `${name}.js`
         else return `${name}.${format}`
@@ -16,9 +16,5 @@ export default defineConfig({
       formats: ["es", "cjs"],
     },
   },
-  plugins: [
-    nodeExternals(),
-    versionPlugin(),
-    swc(),
-  ],
+  plugins: [nodeExternals(), versionPlugin(), swc()],
 })
